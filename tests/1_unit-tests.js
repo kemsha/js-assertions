b15e39e9e1c786953a001853a0c09c1d0eb8c23d
kemsha <arijan.komsic@stu.ibu.edu.ba>
2024-11-05 12:24:46 +0000

    Third unit test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -16,9 +16,9 @@ suite('Unit Tests', function () {
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOK("I'm truthy", 'A string is truthy');
+      assert.isOK(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
